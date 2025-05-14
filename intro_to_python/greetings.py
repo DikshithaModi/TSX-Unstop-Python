41b2de2b36dd08eab9ff2b1ea9f542dfbefb283f
Dikshitha <modidikshitha@gmail.com>
2025-05-14 10:20:44 +0530

    Add intro_to_python folder

---
+++ intro_to_python/greetings.py
@@ -0,0 +1,2 @@
+print("Hello this is dikshitha")
+print("Good Morning ")
---
+++ intro_to_python/my_first_script_with_comments.py
@@ -0,0 +1,13 @@
+#The line prints welcome message to user
+#this can be done even by using input command
+print("Welcome to the python world")
+
+#define variable to store your name
+my_name="dikshitha modi"
+
+#personalized greeting using my name
+print("Hello this is "+my_name)
+
+#perform simple calculation
+res=2+3
+print(f"Result: {res}")
